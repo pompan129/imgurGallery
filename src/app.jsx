7fe32e363570c650aabe708e9379066b54a2549e
fazbat <fazbat@gmail.com>
2016-02-13 11:58:20 -0800

    api for fetch, set to test in browser(api.get())

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var Routes = require("./routes");
+var Api = require("./utils/api");
 
 
 
---
+++ src/utils/api.jsx
@@ -0,0 +1,17 @@
+var Fetch = require("whatwg-fetch");
+var rootUrl = "https://api.imgur.com/3/";
+var apiKey = "5ccb6d948fa094b";
+
+module.exports = window.api = {
+    get: function(url){
+       return  fetch(rootUrl + url, {
+            headers: {
+                "Authorization": "Client-ID " + apiKey
+            }
+       }).then(function(response){
+           return response.json();
+       }).then(function(data){
+           console.log(data);
+       })
+    }
+};
